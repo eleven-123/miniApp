234be3ad876f107afd0165e23e08e2ac2713f8b0
eleven-123 <1782981957@qq.com>
2019-02-28 16:51:35 +0800

    add sort

--- wechat-mall/pages/sort/sort.js
+++ wechat-mall/pages/sort/sort.js
@@ -1,18 +1,78 @@
 // pages/sort/sort.js
-Page({
 
+const app = getApp();
+
+Page({
   /**
    * 页面的初始数据
    */
   data: {
-  
+    currId:0,
+    currCate:'',
+    leftArray:[],
+    rightArray:[],
+    subCateList:[]
   },
-
+  
+  // 获取分类
+  getCategory:function () {
+    var that = this;
+    wx.request({
+      url: app.apiRoutes.getCategory,
+      header: app.apiHeader,
+      success:function(res){
+        var leftList=[],
+            subCateList=[];
+        var data = res.data.data;
+        for (var i=0; i < data.length; i++){data
+          if(data[i].level==1){
+            leftList.push(data[i]);
+          }
+          if(data[i].pid!=0){
+            subCateList.push(data[i]);
+          }
+        }
+        that.setData({
+          currId : data[0].id,
+          currCate : data[0].name,
+          leftArray : leftList,
+          subCateList : subCateList
+        })
+        that.getSubCategory(that.data.currId)
+      }
+    })
+  },
+  //获取子分类
+  getSubCategory:function(id){
+    var self = this;
+    var rightList=[];
+    var data = self.data.subCateList;
+    for (var i=0; i < data.length; i++){
+      if(data[i].pid == id){
+        rightList.push(data[i]);
+      }
+    }
+    self.setData({
+      rightArray : rightList
+    })
+  },
+  //切换分类 
+  bindCategoryTab:function(e){
+    var self = this;
+    var id = e.currentTarget.dataset.id;
+    var name = e.currentTarget.dataset.name;
+    self.setData({
+      currId : id,
+      currCate : name
+    })
+    self.getSubCategory(self.data.currId)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var self = this;
+    self.getCategory()
   },
 
   /**
